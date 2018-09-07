87713ed6d8b20ab73fcd6f4a4a08f3277b24f7b1
Gualtiero Frigerio <gualtiero.frigerio@epress.it>
2018-09-07 17:39:58 +0200

    first commit

---
+++ GFBarcodeScanner/Classes/GFBarcodeScanner.swift
@@ -0,0 +1,8 @@
+//
+//  GFBarcodeScanner.swift
+//  GFBarcodeScanner
+//
+//  Created by Gualtiero Frigerio on 31/08/2018.
+//
+
+import Foundation
---
+++ GFBarcodeScanner/Classes/GFBarcodeScannerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GFBarcodeScannerViewController.swift
+//  GFBarcodeScanner
+//
+//  Created by Gualtiero Frigerio on 31/08/2018.
+//
+
+import Foundation
