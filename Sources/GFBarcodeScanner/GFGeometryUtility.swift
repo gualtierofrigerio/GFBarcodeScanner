01b4fe69c25316e5166ceb3d5604ad7ff4694f65
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-10-05 16:06:19 +0200

    refactoring and added documentation

--- Sources/GFBarcodeScanner/GFGeometryUtility.swift
+++ Sources/GFBarcodeScanner/GFGeometryUtility.swift
@@ -9,9 +9,21 @@ import Foundation
 import CoreGraphics
 import UIKit
 
+
+/// This class contains an utility function to get a layer with an array of rectangles
+/// Is used by GFBarcodeScanner to draw a rectangle over an area where a
+/// barcode is detected
 class GFGeometryUtility {
     
-    public static func getLayer(withRectangles rectangles:[CGRect], frameSize:CGRect, strokeColor:CGColor) -> CAShapeLayer {
+    /// Creates a CAShapeLayer with an array of rectangles
+    /// - Parameters:
+    ///   - rectangles: The array of rectangles to draw in the layer
+    ///   - frameSize: the size of the layer
+    ///   - strokeColor: stroke color for the rectangles
+    /// - Returns: A CAShapeLayer with the rectangles
+    public static func getLayer(withRectangles rectangles:[CGRect],
+                                frameSize:CGRect,
+                                strokeColor:CGColor) -> CAShapeLayer {
         let layer = CAShapeLayer()
         layer.frame = frameSize
         
@@ -25,11 +37,13 @@ class GFGeometryUtility {
         return layer
     }
     
-}
-
-// MARK: - Private
-
-extension GFGeometryUtility {
+    // MARK: - Private
+    
+    /// Draws a UIBezierPath on a layer
+    /// - Parameters:
+    ///   - path: The UIBezierPath to draw
+    ///   - layer: The layer where the path has to be drawn
+    ///   - strokeColor: The stroke color of the path
     static private func drawPath(_ path:UIBezierPath, onLayer layer:CAShapeLayer, strokeColor:CGColor) {
         path.close()
         layer.path = path.cgPath
@@ -37,9 +51,13 @@ extension GFGeometryUtility {
         layer.fillColor = UIColor.clear.cgColor
     }
     
-    // The CGRect coming from AV doesn't have the real size but is scaled between 0 and 1
-    // so we need to convert it to real sizes based on the given frame
-    // AV rect is landscape so if we're portrait we invert width and height
+    /// The CGRect coming from AV doesn't have the real size but is scaled between 0 and 1
+    /// so we need to convert it to real sizes based on the given frame
+    /// AV rect is landscape so if we're portrait we invert width and height
+    /// - Parameters:
+    ///   - bounds: the rect bounds
+    ///   - frame: the containing frame
+    /// - Returns: the rect transformed
     static private func transformRect(_ bounds:CGRect,  forFrame frame:CGRect) -> CGRect {
         var returnFrame = CGRect(x: 0, y: 0, width:0, height: 0)
         var size = frame.size
@@ -56,6 +74,12 @@ extension GFGeometryUtility {
         return returnFrame
     }
     
+    /// Update a UIBezierPath with a CGRect
+    /// If no path is specified a new one is created
+    /// - Parameters:
+    ///   - path: The optional path to update
+    ///   - rect: The rectangle to draw
+    /// - Returns: The UIBezierPath with the rectangle
     static private func updatePath(_ path:UIBezierPath?, withRect rect:CGRect) -> UIBezierPath {
         let updatedPath = path ?? UIBezierPath()
         updatedPath.move(to: rect.origin)
@@ -64,4 +88,5 @@ extension GFGeometryUtility {
         updatedPath.addLine(to: CGPoint(x: rect.origin.x, y: rect.origin.y + rect.size.height))
         return updatedPath
     }
+    
 }
